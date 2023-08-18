c195eded04d5a3f1ca98744390a22817c3866586
wahlp <wahlp322@gmail.com>
2023-08-18 11:15:04 +0800

    remove comments

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -4,16 +4,8 @@ import ssl
 
 import sqlalchemy as sa
 from aiomysql.sa import create_engine
-# from sqlalchemy import Table, DDL
 
-# from src.lib.tables import metadata
 from src.lib.tables import reactions, phrases, phrase_usage
-
-# def create_table_ddl(table: Table):
-#     ddl = DDL(f"CREATE TABLE IF NOT EXISTS {table.name} ({table})")
-#     return ddl
-
-# ddl = create_table_ddl(reactions)
 
 async def main():
     ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
@@ -26,13 +18,10 @@ async def main():
         db=os.getenv("DATABASE"),
         loop=asyncio.get_event_loop(),
         ssl=ctx,
-        # ddl=ddl,
     )
 
     async with engine.acquire() as conn:
-        # metadata.create_all(conn)
         for table in [reactions, phrases, phrase_usage]:
-            # if not sa.inspect(engine).has_table(table.name):
             try:
                 stmt = sa.schema.CreateTable(table)
                 await conn.execute(stmt)
@@ -44,5 +33,4 @@ async def main():
     
 
 if __name__ == '__main__':
-    # print(ddl)
     asyncio.run(main())
